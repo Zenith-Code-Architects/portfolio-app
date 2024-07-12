25c6d808e9e9864f9686ef189bbc3a4f768a1092
lilian <rilian0031@gmail.com>
2024-07-12 15:01:22 +0000

    added new images

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,12 +1,101 @@
 import React from 'react'
+import PhoneImage from "./../../assets/images/phone-image.jpg"
+import { Link } from 'react-router-dom'
+import { useForm } from "react-hook-form";
 
-const Login
-  = () => {
-    return (
-      <div>Login
+
+
+const Login = () => {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className='flex h-screen '>
+      <div className="mx-auto w-fit flex flex-col items-start gap-y-4 p-20">
+
+        <div className="flex flex-col">
+          <p className="text-center font-bold text-4xl mb-2">Welcome Back !</p>
+          <h6 className='text-center'>Log in to your portfolio</h6>
+
+          <form className='flex flex-col gap-y-4 mt-4' onSubmit={handleSubmit(onSubmit)}>
+
+            <div className='w-full'>
+              <label htmlFor='email' className=' block text-sm font-bold'>Your Email or Username</label>
+              <input type='text'
+                id="email"
+                name="email" className=" w-[500px] mt-1 p-2 border border-gray-300 rounded w- max-w-full left-0 top-0"
+                {
+                ...register("email", { required: "Email or username is required" })
+
+                }
+
+              />
+              {errors.email && (
+                <p className='text-red-500'>{errors.email.message}</p>
+              )}
+            </div>
+
+
+
+            <div className='w-full'>
+              <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
+              <input type="password"
+                id="password"
+                name="password" className=" w-[500px] mt-1 p-2 border border-gray-300 rounded left-0 top-0"
+                {
+                ...register("password", { required: "Password is required" })
+
+                }
+              />
+              {errors.password && (
+                <p className='text-red-500'>{errors.password.message}</p>
+              )}
+            </div>
+
+
+
+
+            <div className="flex items-center justify-between">
+              <div className='flex items-center'>
+                <input type="checkbox"
+                  id="remember-me"
+                  name="remember-me" className="mr-2" />
+                <label htmlFor="remember-me" className="text-sm font-medium text-gray-700">Remember me</label>
+              </div>
+
+            </div>
+            <div className="">
+              <Link to="#" className="text-sm text-gray-600 hover:underline ">Forgot password?</Link>
+            </div>
+
+
+            <button type="submit" className="mt-4 py-3 px-8 bg-blue-600 text-white rounded-md ">Log In</button>
+            <div className="mt-4 text-sm text-gray-600">
+              Don't have an account? <a href="#" className="text-blue-600 hover:underline">Register</a>
+            </div>
+
+
+          </form>
+        </div>
+      </div>
+      <div className="w-1/2">
+        <img src={PhoneImage} alt="phone image"
+          className='bg-cover bg-center h-screen' />
+
 
       </div>
-    )
-  }
+
+
+
+
+
+
+    </div>
+
+
+  )
+}
 
 export default Login
