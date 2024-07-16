4945bb222954bee6a3836946d71cf8ba16661260
lilian <rilian0031@gmail.com>
2024-07-16 09:11:58 +0000

    footer section updated

--- src/pages/PreviewPage/components/Footer.jsx
+++ src/pages/PreviewPage/components/Footer.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+    <footer className="bg-gray-900 text-white py-6">
+      <div className="container mx-auto px-4 text-center">
+        <p>&copy; Copyright <strong>Zenith Code Architects</strong>. All Rights Reserved</p>
+        <p>Designed by <a href="https://bootstrapmade.com/" className="text-blue-500">ZCA Group</a></p>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
