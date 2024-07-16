56fbfde9d3a101887c848f449eed24c7178d71be
margaret boye <margaretboye8@gmail.com>
2024-07-16 09:10:43 +0000

    add license form

---
+++ src/pages/Dashboard/pages/Add-new/AddLicense.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+
+const AddLicense = () => {
+  return (
+    <div>
+        
+    <form action="">
+
+    <label htmlFor="">
+        Name of License Obtained
+    </label>
+    <input type="text" />
+
+
+
+    <label htmlFor="">
+        Name of License Obtained
+    </label>
+    <input type="text" />
+
+
+
+    <label htmlFor="">
+        Name of License Obtained
+    </label>
+    <input type="text" />
+
+
+
+    <label htmlFor="">
+        Name of License Obtained
+    </label>
+    <input type="text" />
+
+
+
+    <label htmlFor="">
+        Name of License Obtained
+    </label>
+    <input type="text" />
+
+
+
+    <label htmlFor="">
+        Name of License Obtained
+    </label>
+    <input type="text" />
+
+
+
+
+    <label htmlFor="">
+        Name of License Obtained
+    </label>
+    <input type="text" />
+
+
+    
+    <label htmlFor="">
+        Name of License Obtained
+    </label>
+    <input type="text" />
+
+    </form>
+
+    </div>
+  )
+}
+
+export default AddLicense
