7418125adbcc2f8569fdacef28c364bf1a074b25
margaret boye <margaretboye8@gmail.com>
2024-07-17 13:58:47 +0000

    table added

--- src/pages/Dashboard/pages/Skills.jsx
+++ src/pages/Dashboard/pages/Skills.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PagesLayout from '../layouts/PagesLayout'
 import NavBar from '../../../components/NavBar'
 import { useNavigate } from 'react-router-dom'
+import Table from '../../../components/Table'
 
 const Skills = () => {
   const navigate = useNavigate()
@@ -10,7 +11,7 @@ const Skills = () => {
     <div className=''>
       <NavBar />
       <PagesLayout headerText='Skills' buttonText='Add New Skills' onClick={() => navigate("/dashboard/skills/add-skill")} />
-      
+      <Table/>
     </div>
   )
 }
