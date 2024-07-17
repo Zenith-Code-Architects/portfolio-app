976f0adbcc981b90a99e3d5eab41a8f97d7d04bd
lilian <rilian0031@gmail.com>
2024-07-17 14:20:24 +0000

    updated the navbar and hero section

--- src/pages/PreviewPage/components/Navbar.jsx
+++ src/pages/PreviewPage/components/Navbar.jsx
@@ -5,13 +5,13 @@ import N from "./constants/index"
 const Navbar = () => {
 
   return (
-    <div className='fixed top-0 left-0 bottom-0 px-4 overflow-y-auto z-[997] min-w-48'>
-      <nav>
+    <div className='fixed top-0 left-0 bottom-0 px-4 overflow-y-auto z-[997] min-w-48 text-lime-800'>
+      <nav className='flex flex-col space-y-4 mt-6'>
         {N.NAVLINKS.map((item, index) => {
 
           return (
             <Link key={index} to={item.path}>
-              <span className=''>{item.Icon}</span>
+              <span className='text-xl'>{item.Icon}</span>
             </Link>
           )
 
