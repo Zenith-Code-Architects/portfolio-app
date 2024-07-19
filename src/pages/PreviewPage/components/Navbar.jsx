fa44ea3e6acabca0a4f371bb7145a274017ac234
lilian <rilian0031@gmail.com>
2024-07-19 09:35:27 +0000

    added images and changes to the navbar

--- src/pages/PreviewPage/components/Navbar.jsx
+++ src/pages/PreviewPage/components/Navbar.jsx
@@ -5,22 +5,18 @@ import N from "./constants/index"
 const Navbar = () => {
 
   return (
-    <div className='fixed top-0 left-0 bottom-0 px-4 overflow-y-auto z-[997] min-w-48 text-lime-800'>
-      <nav className='flex flex-col space-y-4 mt-6'>
-        {N.NAVLINKS.map((item, index) => {
-
-          return (
-            <Link key={index} to={item.path}>
-              <span className='text-xl'>{item.Icon}</span>
-            </Link>
-          )
-
-        }
-
-        )
-
-        }
-
+    <div className="fixed top-0 left-0 bottom-0 px-2 py-8 overflow-y-auto z-[997] w-32 bg- text-black rounded-r-lg shadow-lg m-2">
+      <nav className="flex flex-col space-y-6 mt-6">
+        {N.NAVLINKS.map((item, index) => (
+          <Link
+            key={index}
+            to={item.path}
+            className="flex items-center space-x-3 p-2 hover:bg-lime-300 rounded "
+          >
+            <span className="text-xl">{item.Icon}</span>
+            <span className="text-lg">{item.name}</span>
+          </Link>
+        ))}
       </nav>
     </div>
   )
